6c494b847e0e0ec25086e30996d3849e7aa0a591
Masaki Hirokawa <info@dolice.net>
2013-07-03 19:10:30 +0900

    First commit

---
+++ DCMp3PlayerSample/DCMp3PlayerSample/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  DCMp3PlayerSample
+//
+//  Created by 廣川政樹 on 2013/07/03.
+//  Copyright (c) 2013年 Dolice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+
+@interface ViewController : UIViewController
+
+@end
